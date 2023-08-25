75a94b479282c1d4c787635495c62df302835e3a
ProBook <usopked16496@gmail.com>
2023-08-25 13:08:31 +0900

    도메인

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['3.35.143.253']
+ALLOWED_HOSTS = ['3.35.143.253', 'daegeonsimsim.store']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
